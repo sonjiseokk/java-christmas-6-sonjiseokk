f5c127ab54a2c9d7b7107fe5002ec513d4a13cd2
sonjiseokk <thswltjr11@gmail.com>
2023-11-10 17:46:51 +0900

    feat: DateRange, EventMonth 상수 추가

---
+++ src/main/java/christmas/constant/EventMonth.java
@@ -0,0 +1,21 @@
+package christmas.constant;
+
+public enum EventMonth {
+    DECEMBER_2023(2023, 12);
+
+    private final int year;
+    private final int month;
+
+    EventMonth(final int year, final int month) {
+        this.year = year;
+        this.month = month;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+}
