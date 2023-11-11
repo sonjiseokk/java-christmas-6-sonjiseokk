23f06b764b64de053cd10cd45eea183c32997bd0
sonjiseokk <thswltjr11@gmail.com>
2023-11-11 22:51:47 +0900

    fix: InputView에서 valid 하도록 수정

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -1,5 +1,6 @@
 package christmas.view;
 
+import christmas.domain.validator.DateValidator;
 import christmas.utils.Parser;
 
 import java.util.Map;
@@ -9,7 +10,9 @@ import static camp.nextstep.edu.missionutils.Console.readLine;
 public class InputView {
     public static String readDate() {
         System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
-        return readLine();
+        String date = readLine();
+        DateValidator.valid(date);
+        return date;
     }
     public static Map<String,Integer> orderMenu() {
         System.out.println("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
