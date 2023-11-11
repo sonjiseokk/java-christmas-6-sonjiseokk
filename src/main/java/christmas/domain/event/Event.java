e07958a853df8befcb7a576a8c4fc2cff57bee44
sonjiseokk <thswltjr11@gmail.com>
2023-11-11 19:22:24 +0900

    feat: Event enum 생성

---
+++ src/main/java/christmas/domain/event/Event.java
@@ -0,0 +1,16 @@
+package christmas.domain.event;
+
+public enum Event {
+    D_DAY_DISCOUNT("크리스마스 디데이 할인: ",1_000),
+    WEEKDAY("평일 할인: ",2_023),
+    WEEKEND("특별 할인: ",2_023),
+    GIFT("증정 이벤트: ", 25_000);
+
+    private String keyword;
+    private Integer discount;
+
+    Event(final String keyword, final Integer discount) {
+        this.keyword = keyword;
+        this.discount = discount;
+    }
+}
