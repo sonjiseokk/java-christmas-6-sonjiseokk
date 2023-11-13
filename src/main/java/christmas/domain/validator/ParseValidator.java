6fbcb90f21f5a0d534ab5eced5d40c6c036918f8
sonjiseokk <thswltjr11@gmail.com>
2023-11-13 14:16:32 +0900

    refactor: ParseValidator 기능 분리 및 음수 Valid 추가

--- src/main/java/christmas/domain/validator/ParseValidator.java
+++ src/main/java/christmas/domain/validator/ParseValidator.java
@@ -1,11 +1,28 @@
 package christmas.domain.validator;
 
 public class ParseValidator {
-    public static void valid(final String[] itemParts) {
+    public static void typeValid(final String[] itemParts) {
         if (isNotNum(itemParts[1])) {
             throw new IllegalArgumentException("유효하지 않은 주문입니다. 다시 입력해 주세요.");
         }
+        if (isSatisfyOne(itemParts[1])) {
+            throw new IllegalArgumentException("유효하지 않은 주문입니다. 다시 입력해 주세요.");
+        }
     }
+    public static void commaValid(final String[] itemParts) {
+        if (isEmpty(itemParts)) {
+            throw new IllegalArgumentException("유효하지 않은 주문입니다. 다시 입력해 주세요.");
+        }
+    }
+
+    private static boolean isSatisfyOne(final String itemPart) {
+        return Integer.parseInt(itemPart) <= 0;
+    }
+
+    private static boolean isEmpty(final String[] itemParts) {
+        return itemParts.length == 0;
+    }
+
     private static boolean isNotNum(String number) {
         String regex = "^[0-9]+$";
         return !number.matches(regex);
