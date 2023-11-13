0b8a6d96918568674be08f03a713d61753ab0e3c
sonjiseokk <thswltjr11@gmail.com>
2023-11-13 14:31:13 +0900

    fix: 콤마가 연속적으로 들어오는 경우 valid

--- src/main/java/christmas/domain/validator/ParseValidator.java
+++ src/main/java/christmas/domain/validator/ParseValidator.java
@@ -1,24 +1,21 @@
 package christmas.domain.validator;
 
 public class ParseValidator {
+        public static void commaValid(final String[] itemParts) {
+            if (isEmpty(itemParts)) {
+                throw new IllegalArgumentException("유효하지 않은 주문입니다. 다시 입력해 주세요.");
+            }
+            for (String part : itemParts) {
+                if (part.trim().isEmpty()) {
+                    throw new IllegalArgumentException("유효하지 않은 주문입니다. 다시 입력해 주세요.");
+                }
+            }
+        }
     public static void typeValid(final String[] itemParts) {
         if (isNotNum(itemParts[1])) {
             throw new IllegalArgumentException("유효하지 않은 주문입니다. 다시 입력해 주세요.");
         }
-        if (isSatisfyOne(itemParts[1])) {
-            throw new IllegalArgumentException("유효하지 않은 주문입니다. 다시 입력해 주세요.");
-        }
     }
-    public static void commaValid(final String[] itemParts) {
-        if (isEmpty(itemParts)) {
-            throw new IllegalArgumentException("유효하지 않은 주문입니다. 다시 입력해 주세요.");
-        }
-    }
-
-    private static boolean isSatisfyOne(final String itemPart) {
-        return Integer.parseInt(itemPart) <= 0;
-    }
-
     private static boolean isEmpty(final String[] itemParts) {
         return itemParts.length == 0;
     }
