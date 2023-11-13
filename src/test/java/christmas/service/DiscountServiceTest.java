a15307383d13fb6a3bb51a61bf9ce06024dae5e7
sonjiseokk <thswltjr11@gmail.com>
2023-11-13 18:37:58 +0900

    fix: 12월에는 해당 케이스가 없어 삭제

--- src/test/java/christmas/service/DiscountServiceTest.java
+++ src/test/java/christmas/service/DiscountServiceTest.java
@@ -1,11 +1,13 @@
 package christmas.service;
 
+import camp.nextstep.edu.missionutils.Console;
 import christmas.domain.event.Benefit;
 import christmas.domain.order.Order;
 import christmas.service.policy.ChristmasDayPolicy;
 import christmas.service.policy.SpecialPolicy;
 import christmas.service.policy.WeekPolicy;
 import christmas.view.InputView;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -18,8 +20,11 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class DiscountServiceTest {
     private final Integer SPECIAL_AND_WEEKDAY_DATE = 3;
-    private final Integer SPECIAL_AND_WEEKEND_DATE = 25;
     private final Integer WEEKEND_AND_CHRISTMAS_DATE = 24;
+    @AfterEach
+    void after() {
+        Console.close();
+    }
 
     @Test
     @DisplayName("평일 할인과 특별 할인이 중복된 경우")
@@ -34,20 +39,6 @@ class DiscountServiceTest {
         //then
 
         assertThat(discountService.totalDiscount(benefits)).isEqualTo(3023);
-    }
-    @Test
-    @DisplayName("주말 할인과 특별 할인이 중복된 경우")
-    void 주말_할인과_특별_할인이_중복된_경우() throws Exception {
-        //given
-        Order order = getOrder(SPECIAL_AND_WEEKEND_DATE, "티본스테이크-1,바비큐립-1,초코케이크-1,제로콜라-1");
-        //when
-        DiscountService discountService = new DiscountService(
-                List.of(new WeekPolicy(), new SpecialPolicy())
-        );
-        List<Benefit> benefits = discountService.discount(order);
-        //then
-
-        assertThat(discountService.totalDiscount(benefits)).isEqualTo(5046);
     }
     @Test
     @DisplayName("크리스마스 할인과 평일 할인이 중복된 경우")
