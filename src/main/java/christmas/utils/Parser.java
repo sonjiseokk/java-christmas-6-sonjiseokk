7b832c06aca5f88319b388a0c8eaca5384783046
sonjiseokk <thswltjr11@gmail.com>
2023-11-13 14:17:22 +0900

    refactor: ParseValidator 기능 추가 및 공백 삭제

--- src/main/java/christmas/utils/Parser.java
+++ src/main/java/christmas/utils/Parser.java
@@ -8,23 +8,25 @@ import java.util.Map;
 public class Parser {
     public static Map<String, Integer> parse(String menuInput) {
         String[] parts = menuInput.split(",");
-        return generateMenus(parts);
+        ParseValidator.commaValid(parts);
+        return generateMenusValues(parts);
     }
 
-    private static Map<String, Integer> generateMenus(final String[] parts) {
-        Map<String, Integer> menus = new HashMap<>();
+    private static Map<String, Integer> generateMenusValues(final String[] parts) {
+        Map<String, Integer> menuValues = new HashMap<>();
         for (String part : parts) {
-            processPart(part, menus);
+            part = part.trim();
+            processPart(part, menuValues);
         }
-        return menus;
+        return menuValues;
     }
-    private static void processPart(String part, Map<String, Integer> menus) {
+    private static void processPart(String part, Map<String, Integer> menuValues) {
         String[] itemParts = part.split("-");
-        ParseValidator.valid(itemParts);
+        ParseValidator.typeValid(itemParts);
 
         String name = itemParts[0].trim();
         int quantity = Integer.parseInt(itemParts[1].trim());
 
-        menus.put(name, menus.getOrDefault(name, 0) + quantity);
+        menuValues.put(name, menuValues.getOrDefault(name, 0) + quantity);
     }
 }
